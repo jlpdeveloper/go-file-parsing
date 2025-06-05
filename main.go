c7ec27643499d9faf985e8ce33bd9ceda86ce8ca
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-06-04 20:19:10 -0500

    Add distributed cache support and refactor file parsing
    Introduced a distributed cache client with basic read/write functionality for enhanced data handling. Refactored file parsing logic and updated config loading to use a more specific parser configuration method.

--- main.go
+++ main.go
@@ -2,21 +2,45 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"go-file-parsing/cache"
 	"go-file-parsing/config"
 	"go-file-parsing/validator"
 	"os"
 )
 
 func main() {
-	println("Hello World")
-	file, _ := os.Open("sample.csv")
+	cacheClient, err := cache.New()
+	if err != nil {
+		panic(err)
+	}
+	defer cacheClient.Close()
+
+	readWriteValkey(cacheClient)
+	parseFile("sample.csv")
+
+}
+
+func readWriteValkey(cacheClient cache.DistributedCache) {
+	ctx := context.Background()
+	cacheClient.Set(ctx, "Hello", "World")
+	println("Write Done")
+	val, _ := cacheClient.Get(ctx, "Hello")
+
+	println(val)
+	//cacheClient.Do(ctx, cacheClient.B().Hsetnx().Key("test").Field("total").Value("1").Build())
+	//cacheClient.Do(ctx, cacheClient.B().Hincrby().Key("test").Field("total").Increment(10).Build())
+}
+
+func parseFile(filename string) {
+	file, _ := os.Open(filename)
 	defer func() {
 		fcErr := file.Close()
 		if fcErr != nil {
 			panic(fcErr)
 		}
 	}()
-	conf, err := config.LoadConfig("config.json")
+	conf, err := config.LoadParserConfig("config.json")
 	if err != nil {
 		panic(err)
 	}
