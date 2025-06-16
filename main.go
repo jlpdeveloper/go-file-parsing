b87ce08cb478565155757b6612ab5865f44937d4
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-06-15 19:00:47 -0500

    Refactored CSV parsing to include timing and updated file path.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"context"
-	"fmt"
 	"go-file-parsing/cache"
 	"go-file-parsing/config"
 	"go-file-parsing/loan_info"
@@ -11,6 +10,7 @@ import (
 	"log"
 	"os"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -19,24 +19,12 @@ func main() {
 		panic(err)
 	}
 	defer cacheClient.Close()
-
+	start := time.Now()
 	//readWriteValkey(cacheClient)
-	parseFile("sample.csv", cacheClient)
-
-}
-
-func readWriteValkey(cacheClient cache.DistributedCache) {
-	ctx := context.Background()
-	err := cacheClient.Set(ctx, "Hello", "World")
-	if err != nil {
-		panic(err)
-	}
-	println("Write Done")
-	val, _ := cacheClient.Get(ctx, "Hello")
-
-	println(val)
-	//cacheClient.Do(ctx, cacheClient.B().Hsetnx().Key("test").Field("total").Value("1").Build())
-	//cacheClient.Do(ctx, cacheClient.B().Hincrby().Key("test").Field("total").Increment(10).Build())
+	//parseFile("sample.csv", cacheClient)
+	parseFile("data/accepted_2007_to_2018Q4.csv", cacheClient)
+	end := time.Now()
+	log.Printf("Time elapsed: %s", end.Sub(start))
 }
 
 func parseFile(filename string, cacheClient cache.DistributedCache) {
@@ -54,10 +42,21 @@ func parseFile(filename string, cacheClient cache.DistributedCache) {
 	if err != nil {
 		panic(err)
 	}
+	// Create a pool of validators
 	pool := loan_info.NewRowValidatorPool(&conf, cacheClient, 100)
+	// Create a channel to receive errors
 	errChan := make(chan validator.RowError, 100)
 	var rowCount int64 = 0
 	scanner := bufio.NewScanner(file)
+	var errors []validator.RowError
+	errorWg := &sync.WaitGroup{}
+	errorWg.Add(1)
+	go func() {
+		defer errorWg.Done()
+		for err := range errChan {
+			errors = append(errors, err)
+		}
+	}()
 	const maxScannerBufferSize = 1024 * 1024 // 1MB buffer
 	buf := make([]byte, maxScannerBufferSize)
 	scanner.Buffer(buf, maxScannerBufferSize)
@@ -74,7 +73,7 @@ func parseFile(filename string, cacheClient cache.DistributedCache) {
 			defer wg.Done()
 			id, rowErr := rowVal.Validate(row)
 			if rowErr != nil {
-				log.Println(rowErr.Error())
+				//log.Println(rowErr.Error())
 				errChan <- validator.RowError{
 					Row:   rowNum,
 					Id:    id,
@@ -83,17 +82,16 @@ func parseFile(filename string, cacheClient cache.DistributedCache) {
 			}
 			pool <- rowVal
 		}(scanner.Text(), currentRow)
+
+		log.Printf("Processed %d rows\n", currentRow)
+
 		rowCount++
-	}
-	var errors []validator.RowError
-	errorWg := &sync.WaitGroup{}
-	errorWg.Add(1)
-	go func() {
-		defer errorWg.Done()
-		for err := range errChan {
-			errors = append(errors, err)
+		// Missing error check after the loop
+		if err := scanner.Err(); err != nil {
+			log.Fatalf("Error scanning file: %v", err)
 		}
-	}()
+	}
+
 	wg.Wait()
 	log.Println("CSV parsing complete.")
 	close(errChan)
@@ -101,7 +99,7 @@ func parseFile(filename string, cacheClient cache.DistributedCache) {
 	for _, err := range errors {
 		//Cleanup all the bad data
 		_ = cacheClient.Delete(context.Background(), err.Id)
-		log.Println(fmt.Sprintf("error on line: %d, error: %s", err.Row, err.Error.Error()))
+		//log.Println(fmt.Sprintf("error on line: %d, error: %s", err.Row, err.Error.Error()))
 	}
 
 }
